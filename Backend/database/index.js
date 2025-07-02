b779c1e9139e5155d88d5dcf3b285ba9f638209f
arcozubair <arcozubair@gmail.com>
2025-07-02 17:27:06 +0530

    changed uri

--- Backend/database/index.js
+++ Backend/database/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 async function ConnectDB() {
     try {
-        await mongoose.connect("mongodb://localhost:27017/BillingSystem");
+         await mongoose.connect("mongodb+srv://zubair:zubair2024@inventory.zheqesn.mongodb.net/inventorysystem?retryWrites=true&w=majority&appName=inventory");
         console.log("MONGODB connection Successful !! ");
     } catch (error) {
         console.log("MONGO DB ERROR", error);
